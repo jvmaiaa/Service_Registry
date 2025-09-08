d7e610b1cf43e94d9561d903d6a3cfc02022f9ab
jvmaiaa <joaovictormaia482@gmail.com>
2025-09-08 11:25:34 -0300

    ✨[FEAT]: Implementa o endpoint para obter o valor da porta de cada instância que estiver rodando dos MS-Pedidos.

--- pedidos/pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ pedidos/pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -6,6 +6,8 @@ import br.com.alurafood.pedidos.service.PedidoService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.batch.BatchTransactionManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -56,5 +58,10 @@ public class PedidoController {
 
             return ResponseEntity.ok().build();
 
+        }
+
+        @GetMapping("/porta")
+        public String retornaPorta(@Value("${local.server.port}") String porta) {
+            return String.format("Requisição respondida pela instância executando na porta %s", porta);
         }
 }
